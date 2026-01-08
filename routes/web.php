dd7565fbe84708bba297943e9f37aead9b619c6d
marinasvirel <marusenka_83@mail.ru>
2026-01-08 05:21:23 +0300

    удаление use

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('home');
