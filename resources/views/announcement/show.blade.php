5fb634002f6a8bd10245ae99b5f5682ee9b77f54
marinasvirel <marusenka_83@mail.ru>
2026-04-11 01:06:13 +0300

    стилизация выведеных изображений

--- resources/views/announcement/show.blade.php
+++ resources/views/announcement/show.blade.php
@@ -1,17 +1,21 @@
-<div class="announcement-photos">
-    @forelse($announcement->images as $image)
-        <div class="photo-item" style="margin-bottom: 20px;">
-            <img src="{{ asset('storage/' . $image->path) }}" 
-                 alt="Фото к объявлению {{ $announcement->title }}"
-                 style="max-width: 100%; height: auto; border-radius: 8px; display: block;">
-        </div>
-    @empty
+@extends('layout')
+@section('title', $announcement->title)
+@section('content')
+<div class="announcement">
+    <ul class="announcement-photos-list">
+        @forelse($announcement->images as $index => $image)
+        <li class="announcement-photos-item">
+            <img src="{{ asset('storage/' . $image->path) }}" alt="Фото к объявлению {{ $announcement->title }}">
+        </li>
+        @empty
         <p>Фотографии отсутствуют</p>
-    @endforelse
+        @endforelse
+    </ul>
+    <div class="announcement-content">
+        <h1>{{ $announcement->title }}</h1>
+        <p>{{ $announcement->text }}</p>
+        <p>Автор: {{ $announcement->user->name }}</p>
+    </div>
 </div>
 
-<div class="announcement-content">
-    <h1>{{ $announcement->title }}</h1>
-    <p>{{ $announcement->text }}</p>
-    <p>Автор: {{ $announcement->user->name }}</p>
-</div>
+@endsection
